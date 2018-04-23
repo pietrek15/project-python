2c11740d3c1dc89fc5f84b554587898d5cb0fbe0
Piotr W <37346684+pietrek15@users.noreply.github.com>
2018-04-24 00:29:57 +0200

    Add files via upload

--- djangoblog/articles/urls.py
+++ djangoblog/articles/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.article_list),
+    path('',views.article_list, name="list"),
+    path('(?P<slug>[\w-]+)/',views.article_detail,name="detail"),
 ]
