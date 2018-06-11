4329904d4dab3f66e1d105f5c57d6f9986d61790
Piotr W <37346684+pietrek15@users.noreply.github.com>
2018-06-12 00:26:18 +0200

    Add files via upload

--- djangoblog/articles/urls.py
+++ djangoblog/articles/urls.py
@@ -6,6 +6,6 @@ app_name = 'articles'
 urlpatterns = [
     path('',views.article_list, name="list"),
     path('create/',views.article_create,name="create"),
-    path('blog/view/<slug>/',views.article_detail,name="detail"),
+    path('<slug>/',views.article_detail,name="detail"),
 
 ]
